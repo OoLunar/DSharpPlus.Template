049bea7bb2fef00e6b3f319d9597abaac5c033e5
Lunar Starstrum <lunar@forsaken-borders.net>
2023-06-23 23:29:36 -0500

    [ci-skip] Use @ over $ for compatibility with Docker

--- src/Program.cs
+++ src/Program.cs
@@ -7,14 +7,14 @@ using DSharpPlus;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using OoLunar.$RepositoryName.Events;
+using OoLunar.@RepositoryName.Events;
 using Serilog;
 using Serilog.Events;
 using Serilog.Sinks.SystemConsole.Themes;
 using DSharpPlus.CommandAll;
 using DSharpPlus.CommandAll.Parsers;
 
-namespace OoLunar.$RepositoryName
+namespace OoLunar.@RepositoryName
 {
     public sealed class Program
     {
@@ -26,7 +26,7 @@ namespace OoLunar.$RepositoryName
 #if DEBUG
                 .AddJsonFile("config.debug.json", true, true)
 #endif
-                .AddEnvironmentVariables("$RepositoryName_")
+                .AddEnvironmentVariables("@RepositoryName_")
                 .Build());
 
             services.AddSerilog((services, loggerConfiguration) =>
--- tools/replace-variables.sh
+++ tools/replace-variables.sh
@@ -2,11 +2,11 @@
 
 REPOSITORY_NAME=$1
 
-# Replace file names that contain $RepositoryName with the repository name
-find . -name "$RepositoryName" -exec bash -c 'mv "$1" "${1/\$RepositoryName/$2}"' _ {} "$REPOSITORY_NAME" \;
+# Replace file names that contain @RepositoryName with the repository name
+find . -name "*\@RepositoryName*" -exec bash -c 'mv "$1" "${1/\@RepositoryName/$2}"' _ {} "$REPOSITORY_NAME" \;
 
-# Replace file contents that contain $RepositoryName with the repository name
-find . -type f ! -path "./tools/replace-variables.sh" -exec sed -i "s/\$RepositoryName/$REPOSITORY_NAME/g" {} +
+# Replace file contents that contain @RepositoryName with the repository name
+find . -type f -not -path "./tools/replace-variables.sh" -and -not -path "./.git/*" -exec sed -i "s/\@RepositoryName/$REPOSITORY_NAME/g" {} +
 
 # Add all files to git
 git config --global user.email "github-actions[bot]@users.noreply.github.com"
