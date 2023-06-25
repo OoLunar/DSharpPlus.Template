bc98a845ddd0c2902d47fefa1cbc5ed01a1fa4ce
Lunar Starstrum <lunar@forsaken-borders.net>
2023-06-25 14:01:18 -0500

    Use full repo name; Remove all extra files

--- tools/replace-variables.sh
+++ tools/replace-variables.sh
@@ -47,22 +47,24 @@ REPOSITORY_DESCRIPTION="$2"
 find . -name "*@RepositoryName*" -exec bash -c 'mv "$1" "${1//@RepositoryName/$2}"' _ {} "$REPOSITORY_NAME" \;
 
 # Replace file contents with the repository variables
-find . -type f -not -path "./tools/replace-variables.sh" -and -not -path "./.git/*" -exec sed -i "s/@RepositoryNameLower/$(sanitize_variable "$REPOSITORY_NAME_LOWER")/g" {} +
-find . -type f -not -path "./tools/replace-variables.sh" -and -not -path "./.git/*" -exec sed -i "s/@RepositoryFullNameLower/$(sanitize_variable "$REPOSITORY_FULL_NAME_LOWER")/g" {} +
-find . -type f -not -path "./tools/replace-variables.sh" -and -not -path "./.git/*" -exec sed -i "s/@RepositoryOwnerLower/$(sanitize_variable "$REPOSITORY_OWNER_LOWER")/g" {} +
-find . -type f -not -path "./tools/replace-variables.sh" -and -not -path "./.git/*" -exec sed -i "s/@RepositoryName/$(sanitize_variable "$REPOSITORY_NAME")/g" {} +
-find . -type f -not -path "./tools/replace-variables.sh" -and -not -path "./.git/*" -exec sed -i "s/@RepositoryFullName/$(sanitize_variable "$REPOSITORY_FULL_NAME")/g" {} +
-find . -type f -not -path "./tools/replace-variables.sh" -and -not -path "./.git/*" -exec sed -i "s/@RepositoryOwner/$(sanitize_variable "$REPOSITORY_OWNER")/g" {} +
-find . -type f -not -path "./tools/replace-variables.sh" -and -not -path "./.git/*" -exec sed -i "s/@RepositoryDescription/$(sanitize_variable "$REPOSITORY_DESCRIPTION")/g" {} +
+find . -type f -not -path "./tools/replace-variables.sh" -and -not -path "./.git/*" -exec sed -i '
+  s/@RepositoryNameLower/'"$(sanitize_variable "$REPOSITORY_NAME_LOWER")"'/g;
+  s/@RepositoryFullNameLower/'"$(sanitize_variable "$REPOSITORY_FULL_NAME_LOWER")"'/g;
+  s/@RepositoryOwnerLower/'"$(sanitize_variable "$REPOSITORY_OWNER_LOWER")"'/g;
+  s/@RepositoryName/'"$(sanitize_variable "$REPOSITORY_NAME")"'/g;
+  s/@RepositoryFullName/'"$(sanitize_variable "$REPOSITORY_FULL_NAME")"'/g;
+  s/@RepositoryOwner/'"$(sanitize_variable "$REPOSITORY_OWNER")"'/g;
+  s/@RepositoryDescription/'"$(sanitize_variable "$REPOSITORY_DESCRIPTION")"'/g;
+' {} +
 
-# Remove template icon files
-rm -rf res/logo* > /dev/null
+# Remove template files
+rm -rf res/logo* .github/workflows/ tools/replace-variables.sh > /dev/null
+cp -rnT github/ .github/ && rm -rf github/ > /dev/null
 
 # Look for unused variables
-grep -rFl --exclude-dir=tools --exclude-dir=.git --exclude-dir=.github --exclude-dir=github "@" .
-if [ $? -eq 0 ]; then
-    echo "ERROR: Unused variables found."
-    exit 1
+if grep -rFl --exclude-dir=tools --exclude-dir=.git --exclude-dir=.github --exclude-dir=github "@" .; then
+  echo "ERROR: Unused variables found."
+  exit 1
 fi
 
 # Add all files to git
@@ -70,7 +72,3 @@ git config --global user.email "github-actions[bot]@users.noreply.github.com" > 
 git config --global user.name "github-actions[bot]" > /dev/null
 git add . > /dev/null
 git commit -m "[ci-skip] Replace variables." > /dev/null
-
-# Remove this script
-rm -rf .github/workflows/ && cp -rnT github/ .github/ && rm -rf github/ > /dev/null
-rm -rf tools/replace-variables.sh > /dev/null
